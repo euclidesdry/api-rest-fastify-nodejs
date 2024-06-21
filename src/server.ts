04539045714c636c5e230e1f41338cdd71e9a08c
Euclides do Rosário <57298973+euclidesdry@users.noreply.github.com>
2024-06-21 19:23:58 +0100

    feat(routes): ✨ adding transactions route

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.register(transactionsRoutes);
+app.register(transactionsRoutes, {
+  prefix: "transactions",
+});
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP Server is running on port \x1b[32m${env.PORT}\x1b[36m`);
