55bb00d34ea1ff04ad97832a7576ae6b2b52eafd
Euclides do Rosário <57298973+euclidesdry@users.noreply.github.com>
2024-06-21 13:06:18 +0100

    feat(routes): ✨ adding transactions route

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import fastify from "fastify";
 
 import { env } from "./env";
-import { knex } from "./database";
+
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transaction = await knex("transactions")
-    .where("amount", 1000)
-    .select("*");
-
-  return transaction;
-});
+app.register(transactionsRoutes);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP Server is running on port \x1b[32m${env.PORT}\x1b[36m`);
