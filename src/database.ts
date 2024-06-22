dc83919432c8dbfc5039d9a6626ef7c56a691ea5
Euclides do Rosário <57298973+euclidesdry@users.noreply.github.com>
2024-06-22 12:00:38 +0100

    ci: 👷 adding production environment

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,17 @@ import { Knex, knex as setupKnex } from "knex";
 
 import { env } from "./env";
 
+const connection = (
+  env.DATABASE_CLIENT === "sqlite"
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
+) satisfies Knex.Config["connection"];
+
 export const config = {
-  client: "sqlite3",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection,
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
