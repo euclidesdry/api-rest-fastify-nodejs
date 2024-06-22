c6ae8e031cc5ee994e725c919ef86f42b1c0c32e
Euclides do Rosário <57298973+euclidesdry@users.noreply.github.com>
2024-06-22 11:03:26 +0100

    test(routes): ✅ get a specific transaction by id

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -49,5 +49,68 @@ describe("Transactions routes", () => {
         }),
       ]);
     });
+
+    it("should be able to get a specific transaction by id", async () => {
+      const createTransactionResponse = await request(app.server)
+        .post("/transactions")
+        .send({
+          title: "New transaction",
+          amount: 5000,
+          type: "credit",
+        });
+
+      const cookies = createTransactionResponse.get("Set-Cookie") || [];
+
+      const listTransactionsResponse = await request(app.server)
+        .get("/transactions")
+        .set("Cookie", cookies)
+        .expect(200);
+
+      const transactionId = listTransactionsResponse.body.transactions[0].id;
+
+      const getTransactionsResponse = await request(app.server)
+        .get(`/transactions/${transactionId}`)
+        .set("Cookie", cookies)
+        .expect(200);
+
+      expect(getTransactionsResponse.body.transaction).toEqual([
+        expect.objectContaining({
+          title: "New transaction",
+          amount: 5000,
+        }),
+      ]);
+    });
+
+    it("should be able to get the summary", async () => {
+      const createTransactionResponse = await request(app.server)
+        .post("/transactions")
+        .send({
+          title: "New transaction",
+          amount: 5000,
+          type: "credit",
+        });
+
+      const cookies = createTransactionResponse.get("Set-Cookie") || [];
+
+      await request(app.server)
+        .post("/transactions")
+        .set("Cookie", cookies)
+        .send({
+          title: "New transaction",
+          amount: 2000,
+          type: "debt",
+        });
+
+      const summaryResponse = await request(app.server)
+        .get("/transactions/summary")
+        .set("Cookie", cookies)
+        .expect(200);
+
+      expect(summaryResponse.body.summary).toEqual([
+        expect.objectContaining({
+          amount: 3000,
+        }),
+      ]);
+    });
   });
 });
